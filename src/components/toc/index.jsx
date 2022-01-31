707598229185beaaab3ca81651b63e5ae8252b47
Woojin Shin <gintooooonic@gmail.com>
2022-02-01 01:11:59 +0900

    style: TOC 스타일 적용
    TOC에 배경과 폰트 크기 등의 스타일을 적용

--- src/components/toc/index.jsx
+++ src/components/toc/index.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 import './index.scss'
 
 export const TOC = ({ toc }) => (
-  <div className="toc" dangerouslySetInnerHTML={{ __html: toc }} />
+  <div className="toc">
+    <h4 className="toc_header">Table of contents</h4>
+    <div dangerouslySetInnerHTML={{ __html: toc }} />
+  </div>
 )
