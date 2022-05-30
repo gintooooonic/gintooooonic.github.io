6886329b136539f324164ea8ec54027d535da43a
Woojin Shin <gintooooonic@gmail.com>
2022-05-31 01:03:31 +0900

    Update gatsby config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: ``,
   author: `gintooooonic`,
   introduction: `웹 프론트엔드와 백엔드 기술을 공부하고 있습니다.`,
-  siteUrl: `https://gintooooonic.github.io`, // Your blog site url
+  siteUrl: `https://gintooooonic.github.io/old-blog`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: `gintooooonic`, // Your GitHub account
@@ -16,7 +16,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'gintooooonic/gintooooonic.github.io', // Your repository for archive comment
+    utterances: '', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
@@ -27,6 +27,6 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: 'UA-205578547-1', // Add your google analytics tranking ID
+  ga: 'x', // Add your google analytics tranking ID
   ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
 }
