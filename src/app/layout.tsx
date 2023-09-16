0b1b3db3ef4e01e4a8d2dd59a51675a13cd52c12
Woody <me@woodyshin.com>
2023-09-16 13:49:19 +0900

    Prettier 설정 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,11 +9,7 @@ export const metadata: Metadata = {
   description: "",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body className={inter.className}>{children}</body>
