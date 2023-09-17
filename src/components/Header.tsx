b95bd3cef654c17b34cca156a5f26ef63be39621
Woody <me@woodyshin.com>
2023-09-17 15:11:11 +0900

    헤더 문구 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="mx-auto flex max-w-xl flex-row justify-between px-5 align-middle">
       <h1 className="select-none text-xl font-bold">
-        <Link href="/">@w.shin</Link>
+        <Link href="/">w.shin</Link>
       </h1>
       <nav>
         <ul className="flex select-none flex-row justify-end align-middle font-medium [&>li]:ml-5">
