c522ecac06a88ad48b7ec468f5c94d3e52ba0c3b
Woody <me@woodyshin.com>
2024-04-07 14:47:22 +0900

    remove hr

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -6,8 +6,8 @@ export default function MdPage({ page }: { page: Page }) {
   return (
     <Layout title={`${page.title} — w.shin`}>
       <h1 className="mb-3 text-3xl font-bold">{page.title}</h1>
-      <p className="text-neutral-500">{page.description}</p>
-      <hr className="mb-10 mt-5" />
+      <p className="mb-15 text-neutral-500">{page.description}</p>
+      <div className="mb-10 mt-5" />
 
       <div
         className="prose overflow-x-scroll break-words"
