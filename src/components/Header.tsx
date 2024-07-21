86c7fba95e4b14efc44047d056181e41a01bf59c
w.shin <thisis@wshin.me>
2024-07-21 23:17:58 +0900

    Revert "useDarkMode 추가"
    This reverts commit 435a2d0d95306d609b0e2134293a9e5b79dd7a9a.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,13 @@
 import Link from "next/link";
 import DarkModeToggleButton from "./DarkModeToggleButton";
-import useDarkMode from "@/services/useDarkMode";
 
 export default function Header() {
-  const { isDarkMode, toggleDarkMode } = useDarkMode();
-
   return (
     <header className="mx-auto flex max-w-2xl flex-row justify-between px-5 align-middle">
       <h1 className="select-none text-xl font-bold">
         <Link href="/">w.shin</Link>
       </h1>
-      <DarkModeToggleButton isDarkMode={isDarkMode} onClick={toggleDarkMode} />
+      <DarkModeToggleButton isDarkMode={true} onClick={() => {}} />
     </header>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,10 @@
-import useDarkMode from "@/services/useDarkMode";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  const { isDarkMode } = useDarkMode();
-
   return (
     <Html lang="ko">
       <Head />
-      <body className={isDarkMode ? "dark-mode" : ""}>
+      <body>
         <Main />
         <NextScript />
       </body>
