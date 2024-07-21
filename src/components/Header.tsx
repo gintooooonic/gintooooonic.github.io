e9d16b16b96596c09cf4d5bf222b9ff5cf50d93d
w.shin <thisis@wshin.me>
2024-07-21 23:17:58 +0900

    Revert "dark mode toggle 버튼 추가"
    This reverts commit 9e344f7080ffb43ea9d58f3c32d698e7a3328c76.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import DarkModeToggleButton from "./DarkModeToggleButton";
 
 export default function Header() {
   return (
@@ -7,7 +6,19 @@ export default function Header() {
       <h1 className="select-none text-xl font-bold">
         <Link href="/">w.shin</Link>
       </h1>
-      <DarkModeToggleButton isDarkMode={true} onClick={() => {}} />
+      <Navigator />
     </header>
+  );
+}
+
+function Navigator() {
+  return (
+    <nav>
+      <ul className="flex select-none flex-row justify-end align-middle font-medium [&>li]:ml-5">
+        {/* <li>
+          <Link href="/">Home</Link>
+        </li> */}
+      </ul>
+    </nav>
   );
 }
